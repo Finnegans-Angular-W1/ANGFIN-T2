cbedf3d97d33116bb94415fb7e703147596aa193
Martin Cordero <37609438+Martin199@users.noreply.github.com>
2023-02-02 19:09:27 -0300

    Revert "Angfint2 53"

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
   declarations: [
-    LoaderComponent
 
   ],
   imports: [
