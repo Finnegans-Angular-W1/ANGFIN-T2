1fd8cb0d9cd4589a128a9b21d7c0b619ad4bf116
Miqueas Gimenez <miquigz02@gmail.com>
2023-02-08 15:42:59 -0300

    refactor: terms service

--- src/app/pages/auth-registro/services/terms.service.ts
+++ src/app/pages/auth-registro/services/terms.service.ts
@@ -1,9 +1,8 @@
-import { ModalInfo } from './../../../shared/interfaces/modal';
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { ModalInfo } from './../../../shared/interfaces/modal';
+
+@Injectable({ providedIn: 'root' })
 export class TermsService {
 
   private _termsTitle: string;
@@ -12,13 +11,13 @@ export class TermsService {
 
   constructor() { 
     this._termsTitle = 'Alkemy Bank';
+    this._subtitle = 'Al regristarse y utilizar nuestra aplicación de wallet digital, aceptas cumplir con los siguientes términos y condiciones:'
     this._termsParagraphs = [
       'Te comprometes a proporcionar información veraz y precisa al momento de registrarte en nuestra plataforma. Cualquier información falsa o engañosa puede resultar en la cancelación de tu cuenta.',
       'Aceptas ser responsable de mantener la confidencialidad de tu contraseña y de cualquier actividad que ocurra en tu cuenta. Notificaremos inmediatamente a las autoridades relevantes en caso de detectar cualquier actividad sospechosa o ilegal en tu cuenta.',
       'Comprendes y aceptas que nuestra aplicación solo te proporciona un servicio de almacenamiento y gestión de criptomonedas, y que somos un tercero independiente a cualquier transacción que realices. No somos responsables de ninguna pérdida o daño que puedas sufrir como resultado de transacciones realizadas en nuestra plataforma.',
       'Aceptas que nuestra aplicación y sus contenidos son propiedad exclusiva de nuestra empresa y están protegidos por las leyes de propiedad intelectual. Está prohibido cualquier uso no autorizado de nuestra plataforma o contenido, incluyendo su reproducción, modificación, distribución o exhibición pública.'
     ]
-    this._subtitle = 'Al regristarse y utilizar nuestra aplicación de wallet digital, aceptas cumplir con los siguientes términos y condiciones:'
   }
 
   getModalInfo():ModalInfo{
@@ -29,5 +28,4 @@ export class TermsService {
     }
     return {... modalInfo};
   }
-
 }
