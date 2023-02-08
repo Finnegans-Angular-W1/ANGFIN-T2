81d54843167e59e5046f2aa5dd45bf6873982b92
Brian Pumarica <brianpumarica@gmail.com>
2023-02-08 11:13:53 -0300

    add Alert Component/Service skeleton

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TitleComponent } from './components/title/title.component';
 import { LoaderComponent } from './components/loader/loader.component';
+import { AlertComponent } from './components/alert/alert.component';
 
 @NgModule({
   declarations: [
     TitleComponent,
-    LoaderComponent
+    LoaderComponent,
+    AlertComponent
   ],
   imports: [
     CommonModule
