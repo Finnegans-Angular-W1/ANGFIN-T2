7028b491d6c2ba37c072e8c328a24deb29d3cf13
Yesi90 <yesica.guadalupe.fernandez@gmail.com>
2023-02-08 14:43:50 -0300

    Add: Componente Ganancia Inversion, card donde se muestra la informacion

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,9 +4,10 @@ import { FooterComponent } from "./footer/footer.component";
 import { HomeComponent } from "./components/home/home.component";
 import { HomeRoutingModule } from "./home-routing.module";
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { GananciaInversionComponent } from './components/inversion/ganancia-inversion/ganancia-inversion.component';
 
 @NgModule({
-  declarations: [HomeComponent, FooterComponent, SidebarComponent],
+  declarations: [HomeComponent, FooterComponent, SidebarComponent, GananciaInversionComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
