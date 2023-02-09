c32686433b3518a3bd90bdfafd69dbd070cfc984
Miqueas Gimenez <miquigz02@gmail.com>
2023-02-08 21:27:30 -0300

    feat: cambiar-moneda component, first changes

---
+++ src/app/pages/home/components/cambiar-moneda/cambiar-moneda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cambiar-moneda',
+  templateUrl: './cambiar-moneda.component.html',
+  styleUrls: ['./cambiar-moneda.component.scss']
+})
+export class CambiarMonedaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
