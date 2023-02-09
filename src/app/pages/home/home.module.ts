9aa36fbf50c7defd385425d8fd30d36775d17292
Pablo Pérez <pablo.eluniversoweb@gmail.com>
2023-02-08 21:49:54 -0300

    Componente envio de dinero

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,26 +1,22 @@
-import { SharedModule } from 'src/app/shared/shared.module';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import { CommonModule } from "@angular/common";
 import { FooterComponent } from "./footer/footer.component";
 import { HomeComponent } from "./components/home/home.component";
 import { HomeRoutingModule } from "./home-routing.module";
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { TransactionsFormComponent } from './components/transactions-form/transactions-form.component';
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { SendMoneyComponent } from "./components/send-money/send-money.component";
+import { SharedModule } from "src/app/shared/shared.module";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { TransactionsFormComponent } from "./components/transactions-form/transactions-form.component";
 
 @NgModule({
   declarations: [
-    FooterComponent, 
-    HomeComponent, 
-    SidebarComponent, 
-    TransactionsFormComponent
+    FooterComponent,
+    HomeComponent,
+    SidebarComponent,
+    TransactionsFormComponent,
+    SendMoneyComponent,
   ],
-  imports: [
-    CommonModule, 
-    SharedModule,
-    HomeRoutingModule, 
-    ReactiveFormsModule
-  ],
+  imports: [CommonModule, SharedModule, HomeRoutingModule, ReactiveFormsModule],
 })
 export class HomeModule {}
