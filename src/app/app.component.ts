15f87dd0854a6ec4dbf96c18f141eaae473114a1
Miqueas Gimenez <miquigz02@gmail.com>
2023-02-14 19:27:52 -0300

    refactor:loader component, config loader in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { getLoaderShow, getLoaderMessage } from './core/state/states/loaderState/loader.selectors';
 import { Component, OnInit } from '@angular/core';
 
 import { AppState } from 'src/app/core/state/app.state';
@@ -17,11 +18,17 @@ export class AppComponent implements OnInit {
   showAlert$:Observable<boolean>;
   messageAlert$:Observable<string>;
   typeAlert$:Observable<string>;
+
+  showLoader$:Observable<boolean>;
+  messageLoader$:Observable<string>;
   
   constructor(private store:Store<AppState>) { 
     this.showAlert$ = this.store.select(getAlertShow);
     this.messageAlert$ = this.store.select(getAlertMessage);
     this.typeAlert$ = this.store.select(getAlertType);
+
+    this.showLoader$ = this.store.select(getLoaderShow);
+    this.messageLoader$ = this.store.select(getLoaderMessage);
   }
 
   ngOnInit(): void {  }
