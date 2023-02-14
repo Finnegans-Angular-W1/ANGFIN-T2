ae85720969bdc05c89a983589c16189ba7ac05d4
Miqueas Gimenez <miquigz02@gmail.com>
2023-02-13 21:04:49 -0300

    perf: reconfig auth actions

--- src/app/pages/auth-login/state/auth.actions.ts
+++ src/app/pages/auth-login/state/auth.actions.ts
@@ -1,9 +1,16 @@
 import { User } from './../../../core/interfaces/User';
 import { createAction, props } from "@ngrx/store";
 
+export const loginFail = createAction('- [AUTH] LOGIN FAIL -');
+export const authMeFail = createAction('- [AUTH] AUTH ME FAIL -');
 
-export const login = createAction( '- [AUTH] LOGIN - ',  props<{ accessToken:string }>() );
-
+export const loginSuccess = createAction( '- [AUTH] LOGIN SUCCESS - ',  props<{ accessToken:string }>() );
 export const authMe = createAction( '- [AUTH] AUTH ME (INFO USER) -',  props<{ user:User}>() );
 
+//Actions Login
+export const loginStart = createAction('- [AUTH] LOGIN START -', props<{email:string, password:string}>() );
 export const logout = createAction( '- [AUTH] LOGOUT -' );
+
+//TODO: Crear un action para que se ejecute en: DateCreation + 24 horas
+// export const checkTokenExpiration = createAction('- [AUTH] INIT INTERVAL: CHECK TOKEN EXPIRATION -');
+
