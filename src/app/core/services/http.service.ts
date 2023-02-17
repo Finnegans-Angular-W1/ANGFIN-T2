c6dfe702665fb9dd034df6e3f246a8415c6278c2
IgnacioGhibaudo <ignacioaghibaudo@gmail.com>
2023-02-17 16:37:09 -0300

    Feature: servicios PUT y PATCH génericos

--- src/app/core/services/http.service.ts
+++ src/app/core/services/http.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 import { Injectable } from "@angular/core";
+import { ActivationEnd } from "@angular/router";
 import { Observable } from "rxjs";
 
 //import { api_url } from "../../../environments/environment";
@@ -25,6 +26,16 @@ export class HttpService {
 
   public postGeneric<T>(api_url: string, body: T, idParam: string): Observable<T> {
     return this.http.post<T>(api_url + idParam, body);
+  }
+
+  // Método patch y put genérico. 
+  // Requiere de parámetros el endpoint de la url y el id, la información a modificar/agregar, y si es necesario agregar headers.
+  public patch<T>(api_url: string, body: T, id: number, activateHeader:boolean = false): Observable<T> {
+    return this.http.patch<T>(api_url + id, body, activateHeader ? {headers: this._headers}: {})
+  }
+
+  public put<T>(api_url: string, body: T, id: number, activateHeader:boolean = false): Observable<T> {
+    return this.http.put<T>(api_url + id, body, activateHeader ? {headers: this._headers}: {})
   }
 
   public delete<T>(url: string, id:number, activateHeader:boolean = false ):Observable<T> {
