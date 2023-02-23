ef8ed623bfbe915b4a0acecc26e7f1565dfcc440
Yesi90 <yesica.guadalupe.fernandez@gmail.com>
2023-02-22 22:11:33 -0300

    Add componente prestamos, Change: componente inversiones

--- src/app/pages/home/components/prestamos/prestamos.component.ts
+++ src/app/pages/home/components/prestamos/prestamos.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prestamos.component.scss']
 })
 export class PrestamosComponent implements OnInit {
+  private ganancia!: number;
+  private iva: number = 0.21;
+  private fechaActual = new Date ();
+  
+  private plazo!: number;
+  private fechaNueva !: Date;
+  private montoPrestamo !: number;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  prestamo(event: Event) {
+    this.montoPrestamo = Number((<HTMLInputElement>event.target).value);
+  }
 
+  plazoSeleccionado(event: Event) {}
+
+  calcularFechaFinal(){}
+
+  calcularCuota(){}
+
+  calcularTotal(){}
+
+  
 }
