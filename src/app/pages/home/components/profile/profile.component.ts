9dd612c09bbd507312b21a7c8a5c95a0656072a1
Machado Rafael <63523981+Chomskianoam@users.noreply.github.com>
2023-02-22 21:24:04 -0300

    add: traer datos de la API al componente Profile

--- src/app/pages/home/components/profile/profile.component.ts
+++ src/app/pages/home/components/profile/profile.component.ts
@@ -3,6 +3,11 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { ModalState } from 'src/app/shared/states/modalState/modal.state';
 import { openModal } from 'src/app/shared/states/modalState/modal.actions';
+import { User } from 'src/app/core/interfaces/User';
+import { AuthState } from 'src/app/pages/auth-login/state/auth.state';
+import { getUser } from 'src/app/pages/auth-login/state/auth.selectors';
+import { authMe } from 'src/app/pages/auth-login/state/auth.actions';
+import { take } from 'rxjs';
 
 
 @Component({
@@ -12,9 +17,17 @@ import { openModal } from 'src/app/shared/states/modalState/modal.actions';
 })
 export class ProfileComponent implements OnInit {
   modalInfo = { title:'Edición del Usuario', subtitle:'' }
-  constructor(private store: Store<ModalState> ) { }
+  user: User = { } as User;
+
+  constructor(private store: Store<ModalState>, private _store: Store<AuthState> ) { }
 
   ngOnInit(): void {
+    this._store.select(getUser)
+      .pipe(
+        take(1)
+        )
+        .subscribe( (authMe) => { this.user = authMe.first_name, authMe.last_name, authMe.email
+      });
   }
 
   openModal(){
