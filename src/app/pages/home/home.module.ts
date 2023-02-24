0340e8e69fbc460ce6b48e29ce22bb5f061259d7
Pablo Pérez <pablo.eluniversoweb@gmail.com>
2023-02-24 10:33:46 -0300

    Sección contáctenos

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,27 +1,25 @@
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { HomeRoutingModule } from "./home-routing.module";
-import { SharedModule } from 'src/app/shared/shared.module';
 import { CambiarMonedaComponent } from './components/cambiar-moneda/cambiar-moneda.component';
+import { CargarGastosComponent } from "./components/cargar-gastos/cargar-gastos.component";
+import { CargarSaldosComponent } from './components/cargar-saldos/cargar-saldos.component';
+import { CommonModule } from '@angular/common';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { CoreModule } from 'src/app/core/core.module';
+import { DivisasComponent } from './components/divisas/divisas.component';
 import { GananciaInversionComponent } from "./components/inversion/ganancia-inversion/ganancia-inversion.component";
-
 import { HomeComponent } from "./components/home/home.component";
+import { HomeRoutingModule } from "./home-routing.module";
+import { LayoutComponent } from './layout/layout.component';
+import { NavStaticLinkComponent } from './components/sidebar/nav-static-link/nav-static-link.component';
+import { NgModule } from '@angular/core';
+import { PrestamosComponent } from './components/prestamos/prestamos.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SendMoneyComponent } from './components/home/send-money/send-money.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { TipoDeCambioComponent } from './components/tipo-de-cambio/tipo-de-cambio.component';
-import { TransactionsFormComponent } from "./components/transactions-form/transactions-form.component";
-import { DivisasComponent } from './components/divisas/divisas.component';
 import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
-import { SendMoneyComponent } from './components/home/send-money/send-money.component';
-import { LayoutComponent } from './layout/layout.component';
-import { CoreModule } from 'src/app/core/core.module';
-import { CargarSaldosComponent } from './components/cargar-saldos/cargar-saldos.component';
-
-import { CargarGastosComponent } from "./components/cargar-gastos/cargar-gastos.component";
-import { ProfileComponent } from './components/profile/profile.component';
-import { NavStaticLinkComponent } from './components/sidebar/nav-static-link/nav-static-link.component';
-import { PrestamosComponent } from './components/prestamos/prestamos.component';
-
+import { TransactionsFormComponent } from "./components/transactions-form/transactions-form.component";
 
 @NgModule({
   declarations: [
@@ -39,7 +37,8 @@ import { PrestamosComponent } from './components/prestamos/prestamos.component';
     NavStaticLinkComponent,
     CargarGastosComponent,
     ProfileComponent,
-    PrestamosComponent
+    PrestamosComponent,
+    ContactoComponent
   ],
   imports: [
     CommonModule,
--- src/app/pages/home/services/nav-links.service.ts
+++ src/app/pages/home/services/nav-links.service.ts
@@ -1,89 +1,87 @@
-import { NavMobileLink } from './../interfaces/nav-mobile-link';
-import { NAV_SVGS } from './../constants/navLinks';
-import { NavStaticLink } from './../interfaces/nav-static-link';
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { NAV_SVGS } from "./../constants/navLinks";
+import { NavMobileLink } from "./../interfaces/nav-mobile-link";
+import { NavStaticLink } from "./../interfaces/nav-static-link";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class NavLinksService {
-
   navStaticLinks: NavStaticLink[];
   navMobileLinks: NavMobileLink[];
 
-  constructor() { 
+  constructor() {
     this.navMobileLinks = [
       {
-        name: 'Inicio',
-        link: '/inicio',
+        name: "Inicio",
+        link: "/inicio",
       },
       {
-        name: 'Perfil',
-        link: 'inicio/perfil',
+        name: "Perfil",
+        link: "inicio/perfil",
       },
       {
-        name: 'Divisas',
-        link: 'inicio/divisas',
+        name: "Divisas",
+        link: "inicio/divisas",
       },
       {
-        name: 'Ingresos' ,
-        link: 'inicio/ingresos',
+        name: "Ingresos",
+        link: "inicio/ingresos",
       },
       {
-        name: 'Egresos',
-        link: 'inicio/egresos',
+        name: "Egresos",
+        link: "inicio/egresos",
       },
       {
-        name: 'Plazo Fijo',
-        link: 'inicio/plazo-fijo'
+        name: "Plazo Fijo",
+        link: "inicio/plazo-fijo",
       },
       {
-        name: 'Prestamos',
-        link: 'inicio/prestamos'
+        name: "Prestamos",
+        link: "inicio/prestamos",
       },
       {
-        name: 'Contactanos',
-        link: 'TODOHacerModalContacto',
+        name: "Contactanos",
+        link: "inicio/contacto",
       },
     ];
 
     this.navStaticLinks = [
       {
-        name: 'Home',
-        link: '/inicio',
-        svg: NAV_SVGS.home
+        name: "Home",
+        link: "/inicio",
+        svg: NAV_SVGS.home,
       },
       {
-        name: 'Perfil',
-        link: 'inicio/perfil',
-        svg: NAV_SVGS.profile
+        name: "Perfil",
+        link: "inicio/perfil",
+        svg: NAV_SVGS.profile,
       },
       {
-        name: 'Divisas',
-        link: 'inicio/divisas',
-        svg: NAV_SVGS.divisas
+        name: "Divisas",
+        link: "inicio/divisas",
+        svg: NAV_SVGS.divisas,
       },
       {
-        name: 'Ingresos' ,
-        link: 'inicio/ingresos',
-        svg: NAV_SVGS.ingresos
+        name: "Ingresos",
+        link: "inicio/ingresos",
+        svg: NAV_SVGS.ingresos,
       },
       {
-        name: 'Egresos',
-        link: 'inicio/egresos',
-        svg: NAV_SVGS.egresos
+        name: "Egresos",
+        link: "inicio/egresos",
+        svg: NAV_SVGS.egresos,
       },
       {
-        name: 'Plazo Fijo',
-        link: 'inicio/plazo-fijo',
-        svg: NAV_SVGS.plazoFijo
+        name: "Plazo Fijo",
+        link: "inicio/plazo-fijo",
+        svg: NAV_SVGS.plazoFijo,
       },
       {
-        name: 'Prestamos',
-        link: 'inicio/prestamos',
-        svg: NAV_SVGS.prestamos
-      }
+        name: "Prestamos",
+        link: "inicio/prestamos",
+        svg: NAV_SVGS.prestamos,
+      },
     ];
-
   }
 }
